7718d16722fc20799f4342228a7a04208652acae
NguyenTriQuan <nguyentriquan99@gmail.com>
2023-03-17 00:28:41 +0700

    main

--- tests/test_der_example.py
+++ tests/test_der_example.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from utils.main import main, parse_args
+from main import main, parse_args
 import pytest
 
 
