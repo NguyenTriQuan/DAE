630c6e8192f7d70414212ed9e4f4ef09314fe4a4
NguyenTriQuan <nguyentriquan99@gmail.com>
2023-03-23 02:34:38 +0700

    dae

--- backbone/utils/dae_layers.py
+++ backbone/utils/dae_layers.py
@@ -305,9 +305,12 @@ class _DynamicLayer(nn.Module):
             else:
                 self.register_buffer(f'bwt_weight_scale_{i}', torch.ones(1).to(device).view(self.view_in))
 
-            print(getattr(self, f'weight_scale_{i}').view(-1).max())
-            print(getattr(self, f'fwt_weight_scale_{i}').view(-1).max())
-            print(getattr(self, f'bwt_weight_scale_{i}').view(-1).max())
+            temp = getattr(self, f'weight_scale_{i}').view(-1)
+            print(temp.max(), temp.min())
+            temp = getattr(self, f'fwt_weight_scale_{i}').view(-1)
+            print(temp.max(), temp.min())
+            temp = getattr(self, f'bwt_weight_scale_{i}').view(-1)
+            print(temp.max(), temp.min())
 
     def get_optim_params(self):
         params = [self.weight[-1], self.fwt_weight[-1], self.bwt_weight[-1], self.score]
