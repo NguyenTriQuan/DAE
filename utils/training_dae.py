2f215e3799aadd72974005ec555b745325648e48
NguyenTriQuan <nguyentriquan99@gmail.com>
2023-03-23 13:39:53 +0700

    dae

--- utils/training_dae.py
+++ utils/training_dae.py
@@ -202,10 +202,10 @@ def train(model: ContinualModel, dataset: ContinualDataset,
         # train_loop(t, model, dataset, args, progress_bar, train_loader, mode='jr')
 
         print('checking forgetting')
-        accs = evaluate(model, dataset, task=None, mode='kbts')
+        accs = evaluate(model, dataset, task=t, mode='kbts')
         print(f'kbts accs: cil {accs[0]}, til {accs[1]}')
 
-        accs = evaluate(model, dataset, task=None, mode='ets')
+        accs = evaluate(model, dataset, task=t, mode='ets')
         print(f'ets accs: cil {accs[0]}, til {accs[1]}')
 
         # accs = evaluate(model, dataset, task=None, mode='jr')
