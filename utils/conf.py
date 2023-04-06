152592e5c6316d6abe4688a1a32a1e0a13652c01
NguyenTriQuan <nguyentriquan99@gmail.com>
2023-04-07 04:53:05 +0700

    dae

--- utils/conf.py
+++ utils/conf.py
@@ -19,21 +19,21 @@ def base_path() -> str:
     """
     Returns the base bath where to log accuracies and tensorboard data.
     """
-    os.makedirs('/tmp/log/', exist_ok=True)
+    os.makedirs('./tmp/log/', exist_ok=True)
     return './tmp/log/'
 
 def base_path_dataset() -> str:
     """
     Returns the base bath where to log accuracies and tensorboard data.
     """
-    os.makedirs('/tmp/data/', exist_ok=True)
+    os.makedirs('./tmp/data/', exist_ok=True)
     return './tmp/data/'
 
 def base_path_memory() -> str:
     """
     Returns the base bath where to store model and buffer.
     """
-    os.makedirs('/tmp/memory/', exist_ok=True)
+    os.makedirs('./tmp/memory/', exist_ok=True)
     return './tmp/memory/'
 
 
