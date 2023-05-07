4265456a5b55513c6b90d83061eaa46af0977073
NguyenTriQuan <nguyentriquan99@gmail.com>
2023-05-07 21:08:30 +0700

    dae

--- backbone/ResNet18_DAE.py
+++ backbone/ResNet18_DAE.py
@@ -276,7 +276,6 @@ class ResNet(_DynamicModel):
         self.total_strength = 1
         for m in self.DB:
             self.total_strength += m.strength
-            print(m.strength)
 
     def squeeze(self, optim_state):
         mask_in = None
