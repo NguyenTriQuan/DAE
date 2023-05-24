e8f6485889083f716db6eeb4ca094af2652cf382
NguyenTriQuan <nguyentriquan99@gmail.com>
2023-05-24 10:00:12 +0700

    dae

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ def main(args=None):
     setproctitle.setproctitle('{}_{}_{}'.format(args.model, args.buffer_size if 'buffer_size' in args else 0, args.dataset))
     if model.NAME == 'DAE':
         from utils.training_dae import train, evaluate, train_cal
-        args.title = '{}_{}_{}_lamb_{}_drop_{}_sparsity_{}'.format(args.model, args.buffer_size if 'buffer_size' in args else 0, args.dataset, 
+        args.title = '{}_{}_lamb_{}_drop_{}_sparsity_{}'.format(args.model, args.dataset, 
                                                                 args.lamb, args.dropout, args.sparsity)
         print(args.title)
     elif model.NAME == 'ATA':
