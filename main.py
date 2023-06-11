861507538090355fcab60ad221ef4fba8f416a30
NguyenTriQuan <nguyentriquan99@gmail.com>
2023-06-11 10:38:19 +0700

    dae

--- main.py
+++ main.py
@@ -133,8 +133,9 @@ def main(args=None):
     # setproctitle.setproctitle('{}_{}_{}'.format(args.model, args.buffer_size if 'buffer_size' in args else 0, args.dataset))
     if model.NAME == 'DAE':
         from utils.training_dae import train, evaluate, train_cal
-        args.title = '{}_{}_lamb_{}_drop_{}_sparsity_{}_tem_{}_buf_{}'.format(args.model, args.dataset, 
-                                                                args.lamb, args.dropout, args.sparsity, args.temperature, args.buffer_size)
+        args.title = '{}_{}_lamb_{}_drop_{}_sparsity_{}_tem_{}_buf_{}_{}'.format(args.model, args.dataset, 
+                                                                args.lamb, args.dropout, args.sparsity, args.temperature,
+                                                                 args.buffer_size, args.ablation)
         print(args.title)
     elif model.NAME == 'ATA':
         from utils.training_ata import train
@@ -175,6 +176,7 @@ def main(args=None):
         train_cal(model, dataset, args)
     elif isinstance(dataset, ContinualDataset):
         train(model, dataset, args)
+        train_cal(model, dataset, args)
         evaluate(model, dataset, args)
     else:
         assert not hasattr(model, 'end_task') or model.NAME == 'joint_gcl'
